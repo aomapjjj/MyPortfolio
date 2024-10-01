7bc6ed15d6c709cc0aefaaead13c32f98dbcad19
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-10-01 17:01:37 +0700

    Activity

---
+++ portfolio/src/components/Card.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div
+    class="mx-auto max-w-md overflow-hidden rounded-lg bg-white shadow flex flex-col h-full"
+  >
+    <slot name="img"></slot>
+    <div class="p-4 flex-1">
+      <p class="mb-1 text-sm text-primary-500"><slot name="date"></slot></p>
+      <h3 class="text-xl font-medium text-gray-900">
+        <slot name="Header"></slot>
+      </h3>
+      <p class="mt-1 text-gray-500"><slot name="description"></slot></p>
+    </div>
+    <button
+      class="mt-auto px-5 py-2 tracking-wider text-black mb-3"
+      type="button"
+      aria-label="like"
+    >
+      <slot name="readmore"></slot>
+    </button>
+  </div>
+</template>
+
+<style scoped></style>
--- portfolio/src/view/Activities.vue
+++ portfolio/src/view/Activities.vue
@@ -1,11 +1,36 @@
 <script setup>
-import Navbar from '@/components/Navbar.vue';
+import Navbar from "@/components/Navbar.vue"
+import Card from "@/components/Card.vue"
+import activity from "../../data/activity.json"
 </script>
- 
-<template>
-<Navbar />
-</template>
- 
-<style scoped>
 
-</style>
+<template>
+  <div class="bg-gray-100 h-full min-h-screen w-full pb-10">
+    <Navbar />
+    <section>
+      <div
+        class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 mt-10 mb-0"
+      >
+        <Card v-for="(item, index) in activity" :key="index">
+          <template #img>
+            <img :src="item.img" alt="" />
+          </template>
+          <template #date>
+            {{ item.date }}
+          </template>
+          <template #Header>
+            {{ item.name }}
+          </template>
+          <template #description>
+            {{ item.description }}
+          </template>
+          <template #readmore>
+            <a :href="item.website" target="_blank"> Readmore >> </a>
+          </template>
+        </Card>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped></style>
